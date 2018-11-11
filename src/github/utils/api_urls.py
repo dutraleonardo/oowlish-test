7c68c5a0c231f4b34aaddfc3ecedc9b5d3ddddb5
Leonardo <leonardo.uece@gmail.com>
2018-11-10 23:22:48 -0300

    PEP8 problems
    I solved the pep8 problems

--- src/github/utils/api_urls.py
+++ src/github/utils/api_urls.py
@@ -1,8 +1,8 @@
 BASE_URL = 'https://api.github.com/'
-USERS= 'users'
-REPOS  = 'repos'
+USERS = 'users'
+REPOS = 'repos'
 SEARCH = 'search'
 USERS_SEARCH_URL = BASE_URL + SEARCH + '/' + USERS
 REPO_URL = BASE_URL + USERS + '/{0}/' + REPOS
 PAGINATION_URL = 'page={0}&per_page={1}'
-USERS_URL = BASE_URL +  USERS
+USERS_URL = BASE_URL + USERS
